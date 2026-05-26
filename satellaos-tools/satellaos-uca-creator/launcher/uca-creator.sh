d3c68d3bebd50934c717583031c8b075daac2736
satellaos-official <forgottensatella@gmail.com>
2026-05-26 20:34:28 +0300

    Random Commit

---
+++ satellaos-tools/satellaos-uca-creator/launcher/uca-creator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+URL="https://raw.githubusercontent.com/satellaos-official/satellaos-packages/refs/heads/main/satellaos-tools/satellaos-$Name/core/$Name.py"
+File="/tmp/$Name.py"
+Location="/tmp/"
+Name="uca-creator"
+
+if ! wget -q --spider --timeout=5 https://raw.githubusercontent.com 2>/dev/null; then
+    if command -v satellaos-netcheck &> /dev/null; then
+        satellaos-netcheck
+    else
+        echo "Error: Internet not Found"
+    fi
+    exit 1
+fi
+
+rm -rf "$File"
+
+wget -q -O "$File" "$URL"
+
+python3 "$File" "$@"
